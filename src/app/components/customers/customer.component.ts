1acc86304404c1e8bf35f7d2adf5dd76a4d72695
shubhojit <royshubhojit@gmail.com>
2019-03-02 17:11:16 +1100

    custom validations with hardcoded param usin factory fn isn't working.

--- src/app/components/customers/customer.component.ts
+++ src/app/components/customers/customer.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+
 import { Customer } from './customer';
+
+// function ratingRange(c: AbstractControl): { [key: string]: boolean } | null {
+//   if (c.value !== null && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
+//     return { 'range': true };
+//   }
+//   return null;
+// }
 
 @Component({
   selector: 'rf-customer',
@@ -21,7 +29,8 @@ export class CustomerComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       phone: '',
       notification: 'email',
-      sendCatalog: true,
+      // rating: [null, ratingRange],
+      sendCatalog: true
     });
   }
 
